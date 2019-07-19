be8236bbf99d777741fbeff8d6f37f6a233ba7c8
thadler <t.hadler@aol.de>
2019-07-19 16:18:31 +0200

    done?

---
+++ model_application.py
@@ -0,0 +1,24 @@
+import sys
+import numpy as np
+import pickle
+import json
+
+if __name__=='__main__':
+
+    script_name       = sys.argv[0]
+    model_filename    = sys.argv[1]
+    classify_filename = sys.argv[2]
+
+    model = pickle.load(open(model_filename, 'rb'))
+    x_toclassify = np.array(json.loads(classify_filename))
+    if len(x_toclassify.shape)==1:
+        y_pred = model.predict([x_toclassify])
+    else:
+        y_pred = model.predict(x_toclassify)
+
+    print(y_pred)
+    y_pred = [0 if val<0.5 else 1 for val in y_pred]
+    print(y_pred)
+
+#[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
